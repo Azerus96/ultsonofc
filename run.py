1075ca7caac9132e8afecb73505ca48e779dc091
Azerus96 <gencinski1996@gmail.com>
2024-11-26 11:15:13 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Используем переменную PORT, если она есть
+    port = int(os.environ.get('PORT', 10000))  # Используем переменную PORT, если она есть
     socketio.run(app, host='0.0.0.0', port=port, debug=app.config['DEBUG'])
