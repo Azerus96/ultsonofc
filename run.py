b532c47d27e3de4e80bb0fea8030f6d76d6eaae2
Azerus96 <gencinski1996@gmail.com>
2024-11-26 11:31:00 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
-from app import create_app
-from app.web.socket import socketio
+from app import create_app, socketio  # Импортируем socketio из app/__init__.py
 import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # Используем переменную PORT, если она есть
+    port = int(os.environ.get('PORT', 10000))
     socketio.run(app, host='0.0.0.0', port=port, debug=app.config['DEBUG'])
